a713e472b1658ebb3c17aa02fac3eb9420a4aad2
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-17 02:05:50 +0000

    Refactored spinner

---
+++ spinner2.js
@@ -0,0 +1,13 @@
+const spinner = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|  ', '\r\/  ', '\r\-  ', '\r\\   ', '\r|   ' + "\n"];
+let counter = 0;
+spinner.forEach(element => {
+  counter += 200
+  setTimeout(() => {
+  process.stdout.write(element)
+  }, counter)
+});
+
+/**forEach method executes a provided function once for each array element
+ * set counter at 0 to avoid delay for the first element's output
+ * increase counter by .2 seconds */
+
